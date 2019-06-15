be54c302f81faf76b14353624b3f169f84735c8b
JaneSu <jianweisummer@gmail.com>
2019-06-16 00:06:20 +0800

    解决问题？

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,5 @@
 /// <reference path="globals/axios/index.d.ts" />
+/// <reference path="globals/jest/index.d.ts" />
 /// <reference path="globals/react-router-dom/index.d.ts" />
 /// <reference path="globals/react-router/index.d.ts" />
+/// <reference path="globals/webpack-dev-server/index.d.ts" />
