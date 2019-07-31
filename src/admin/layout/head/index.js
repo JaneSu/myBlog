a2fa0a1cdeb03ea62b9286afa78e8af8884afbb3
JaneSu <jianweisummer@gmail.com>
2019-08-01 00:12:00 +0800

    新增背景效果

--- src/admin/layout/head/index.js
+++ src/admin/layout/head/index.js
@@ -2,19 +2,37 @@ import React from 'react'
 import { connect } from 'react-redux'
 import config from '../../../config'
 import PropTypes from 'prop-types'
-
+import { Button } from 'antd'
 import './index.scss'
+import { withRouter } from 'react-router-dom'
 
 class AdminHead extends React.Component {
 	constructor(props) {
 		super(props)
 	}
 
+	goPage(type) {
+		switch (type) {
+			case 'home':
+				this.props.history.push('/front/index')
+		}
+	}
 	render() {
 		return (
 			<div className='admin-head'>
 				<h1 className='title'>{config.website_name}</h1>
-				<span>退出登录</span>
+				<div>
+					<Button
+						type='primary'
+						shape='circle'
+						icon='home'
+						style={{ marginRight: '.2rem' }}
+						onClick={e => {
+							this.goPage('home')
+						}}
+					/>
+					<Button type='primary' shape='circle' icon='logout' />
+				</div>
 			</div>
 		)
 	}
@@ -26,7 +44,9 @@ const mapStateToProps = state => ({})
 
 const mapDispatchToProps = dispatch => ({})
 
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(AdminHead)
+export default withRouter(
+	connect(
+		mapStateToProps,
+		mapDispatchToProps
+	)(AdminHead)
+)
